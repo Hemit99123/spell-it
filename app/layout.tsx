424567a00884176041b68a0f9fd05920eea49bba
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-03 15:55:23 -0400

    added vercel analytics to get stats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <NavBar />
             {children}
+          <Analytics />
         </body>
     </html>
   );
