72238dc66f66c4417549513478f9f97535b436cd
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-03 15:33:46 -0400

    changed lowercasing in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ const About = async () => {
         <div className="flex md:flex-row flex-col-reverse md:items-stretch items-center justify-center mt-12">
             <div className="md:py-20 xl:w-1/2 sm:w-1/2 lg:mr-20 md:mr-6 flex flex-col md:items-end items-center justify-center xl:mr-28">
                 <div className="flex flex-col items-center justify-center">
-                    <h1 className="md:text-5xl text-3xl font-bold text-center text-gray-800 lowercase">Studying made easier</h1>
+                    <h1 className="md:text-5xl text-3xl font-bold text-center text-gray-800">Studying made easier</h1>
                     <p className="sm:w-96 w-full mt-6 text-base leading-6 text-center text-gray-600">Prepare for a spelling bee or a spelling test using our tool!</p>
                     <div className="md:mt-14 mt-12 flex flex-col items-center">
                         <div className="w-20 h-20 bg-white shadow rounded-full flex items-center justify-center" role="img" aria-label="money">
