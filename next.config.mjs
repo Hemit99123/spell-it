d1b75affa0002e5b99f78468bc969c7ea67ebe2d
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-03 15:27:13 -0400

    trying to fix vercel server actions error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      serverActions: true,
+    },
+    // Ensure that Next.js can handle API routes for Next Auth
+    async rewrites() {
+      return [
+        {
+          source: '/api/auth/:path*',
+          destination: '/api/auth/:path*',
+        },
+      ]
+    },
+  };
+  
+  module.exports = nextConfig;
